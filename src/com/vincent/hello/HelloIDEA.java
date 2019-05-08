11236b635fa8a54a80aa4467b03fc4c9281c616d
Vincent <wanglibiao@bonc.com.cn>
2019-05-08 08:50:06 +0800

    Initial commit

---
+++ src/com/vincent/hello/HelloIDEA.java
@@ -0,0 +1,11 @@
+package com.vincent.hello;
+
+/**
+ * @Author Vincent
+ * @datetime 2019/5/8-8:49
+ */
+public class HelloIDEA {
+    public static void main(String[] args) {
+        System.out.println("hello idea");
+    }
+}
