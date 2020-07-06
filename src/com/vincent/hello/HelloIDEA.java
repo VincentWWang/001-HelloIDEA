fda341df87964a2b48533d488579fc7ec321e594
wanglibiao <wang614235383@163.com>
2020-07-06 18:29:07 +0800

    second commit

--- src/com/vincent/hello/HelloIDEA.java
+++ src/com/vincent/hello/HelloIDEA.java
@@ -7,5 +7,6 @@ package com.vincent.hello;
 public class HelloIDEA {
     public static void main(String[] args) {
         System.out.println("hello ideaVincentWWang");
+        System.out.println("hello ideaVincentWWang");
     }
 }
