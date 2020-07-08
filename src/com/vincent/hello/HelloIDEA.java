6e013dff7719b55e07bd3a00c9c00fbe0ebcf44c
wanglibiao <wang614235383@163.com>
2020-07-08 08:30:47 +0800

    third commit

--- src/com/vincent/hello/HelloIDEA.java
+++ src/com/vincent/hello/HelloIDEA.java
@@ -8,6 +8,5 @@ public class HelloIDEA {
     public static void main(String[] args) {
         System.out.println("hello ideaVincentWWang");
         System.out.println("hello ideaVincentWWang");
-        System.out.println("hello ideaVincentWWang");
     }
 }
